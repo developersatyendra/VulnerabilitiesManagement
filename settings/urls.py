81603a1c8e4a21e496b22f318409e743e5d4be8b
Nguyen Tri Tuan <tuanklnew@gmail.com>
2018-11-07 22:32:43 +0700

    v1.0

--- settings/urls.py
+++ settings/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import RedirectView
+
+
 app_name = 'settings'
 urlpatterns = [
-    path('', SettingsView.as_view(), name='settings'),
+    path('', RedirectView.as_view(url='/settings/myaccount'), name='settings'),
     path('myaccount/', login_required(MyAccountView.as_view()), name='MyAccount'),
     path('accountmanagement/', AccountManagementView.as_view(), name='AccountManagement'),
     path('about/', AboutView.as_view(), name='About')
